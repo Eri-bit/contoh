ca99f6f38cda3e5408d4a4aa62a4d981c6ea517c
Ery-bit <94726955+Eri-bit@users.noreply.github.com>
2021-12-07 20:38:38 +0700

    Add files via upload

---
+++ contoh.sh
@@ -0,0 +1,12 @@
+#!bin/sh
+
+clear
+figlet BIO DATA | lolcat
+echo "==================================" | lolcat
+echo " Nama  : Dea Yuliana   " | lolcat
+echo " Kota  : Lampung Timur " | lolcat
+echo " Kelas : 8             " | lolcat
+echo " Umur  : 13 Yo         " | lolcat
+echo " Game  : Free Fire     " | lolcat
+echo "++++++++++++++++++++++++++++++++++" | lolcat
+
